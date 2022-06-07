3c63b651d7642f28e7b566d93d165982468b56b0
zlw <zlw@foxemail.com>
2022-06-07 22:27:19 +0800

    zlw-test

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -2,37 +2,24 @@ import { View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import "./index.scss";
 
-const listMock = [
+const listMock = 
   {
     title: "媒体评宾利车位风波：已成反腐线索",
-  },
-  {
-    title: "媒体评宾利车位风波：已成反腐线索",
-  },
-  {
-    title: "媒体评宾利车位风波：已成反腐线索",
-  },
-  {
-    title: "媒体评宾利车位风波：已成反腐线索",
-  },
-];
+    content: "文字是人类用符号记录表达信息以传之久远的方式和工具。现代文字大多是记录语言的工具。人类往往先有口头的语言后产生书面文字，很多小语种，有语言但没有文字。文字的不同体现了国家和民族的书…"
+  }
 
 const NewsList = () => {
-  function handleClick() {
-    Taro.navigateTo({
-      url:"/pages/article/index.tsx"
-    })
-  }
   return (
     <View className="list">
-      {listMock.map((v, index) => {
+      {listMock.title}
+     {/*  {listMock.map((v, index) => {
         return (
           <View className="item" key={index} onClick={handleClick}>
             <View className="number">{index + 1}</View>
             <View className="title">{v.title}</View>
           </View>
         );
-      })}
+      })} */}
     </View>
   );
 }
