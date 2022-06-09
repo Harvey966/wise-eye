a3b94d6854e6c3281903d90d67ce9477e6846c64
魏航宇 <weihangyu@bytedance.com>
2022-06-09 23:19:40 +0800

    详情页

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -2,17 +2,17 @@ import { View } from "@tarojs/components";
 import Taro from "@tarojs/taro";
 import "./index.scss";
 
-const listMock = 
-  {
-    title: "媒体评宾利车位风波：已成反腐线索",
-    content: "文字是人类用符号记录表达信息以传之久远的方式和工具。现代文字大多是记录语言的工具。人类往往先有口头的语言后产生书面文字，很多小语种，有语言但没有文字。文字的不同体现了国家和民族的书…"
-  }
+const listMock = {
+  title: "媒体评宾利车位风波：已成反腐线索",
+  content:
+    "文字是人类用符号记录表达信息以传之久远的方式和工具。现代文字大多是记录语言的工具。人类往往先有口头的语言后产生书面文字，很多小语种，有语言但没有文字。文字的不同体现了国家和民族的书…",
+};
 
-const NewsList = () => {
+const Article = () => {
   return (
     <View className="list">
       {listMock.title}
-     {/*  {listMock.map((v, index) => {
+      {/*  {listMock.map((v, index) => {
         return (
           <View className="item" key={index} onClick={handleClick}>
             <View className="number">{index + 1}</View>
@@ -22,5 +22,5 @@ const NewsList = () => {
       })} */}
     </View>
   );
-}
-export default NewsList;
+};
+export default Article;
