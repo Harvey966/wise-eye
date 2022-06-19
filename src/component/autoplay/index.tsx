ebf941d20317c8d9e55c534015d6d7c7d80688ff
zlw <zlw@foxemail.com>
2022-06-19 11:43:40 +0800

    feat: 文章详情页及搜索栏优化

---
+++ src/component/autoplay/index.tsx
@@ -0,0 +1,10 @@
+import { AtIcon } from 'taro-ui'
+import "./index.scss";
+
+
+const Autoplay = () => {
+  return (
+    <AtIcon prefixClass='fa' value='play' size='30' color='#F00'></AtIcon>
+  );
+};
+export default Autoplay;
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,23 +1,11 @@
 import { View } from "@tarojs/components";
 import { useState } from "react";
-import Taro from "@tarojs/taro";
-import SearchBox from "../../component/search-box";
 import "./index.scss";
 import NewsList from "../../component/news-list";
 
 export default function HomePage() {
- /*  const [value, setValue] = useState("");
-  const changeSearch = (e) => {
-    console.log(e);
-  };
-  function handleFocus() {
-    Taro.navigateTo({
-      url: "/pages/search/index",
-    });
-  } */
   return (
     <View>
-      <SearchBox />
       <NewsList />
     </View>
   );
